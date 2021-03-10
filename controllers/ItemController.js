c875c6896d174687dc9e3ba50b9e2db6037eaf3e
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-03-10 16:59:24 +0800

    Add output message to view

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -8,20 +8,20 @@ module.exports = {
   },
 
   store: function(req, res) {
-    items.create(req.mysql, req.body, function(err) {
-      res.send("success!");
+    items.create(req.mysql, req.body, function(err, results) {
+      res.send("Inserted row with an ID of: " + results.insertId);
     })
   },
 
   update: function(req, res) {
-    items.update(req.mysql, req.body, req.params.id, function(err) {
-      res.send("success!");
+    items.update(req.mysql, req.body, req.params.id, function(err, results) {
+      res.send('Updated ' + results.affectedRows + ' rows');
     })
   },
 
   destroy: function(req, res) {
-    items.destroy(req.mysql, req.params.id, function(err) {
-      res.send("success!");
+    items.destroy(req.mysql, req.params.id, function(err, results) {
+      res.send('Deleted ' + results.changedRows + ' rows');
     })
   }
 }
